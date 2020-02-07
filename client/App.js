ccd6ce01149b10d98d60ea04f4512f1346eeebe5
David M <d.anthony.massey@gmail.com>
2020-02-07 12:21:09 -0600

    Add default item 3

--- client/App.js
+++ client/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentItem: { id: 52 },
+      currentItem: { id: 3 },
       currentReviewWord: null,
       currentReviewOrder: "random"
     };
