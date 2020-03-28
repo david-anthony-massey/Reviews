67f7dbec2d3397785a4eed5f840d3c4ffd1cc3db
David M <d.anthony.massey@gmail.com>
2020-03-28 17:11:58 -0500

    Change dependencies for Heroku (MySQL)

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require("./db.js");
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../dist")));
